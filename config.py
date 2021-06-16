328edf4310f49f38581e140cbc882cfb0b1f4b2a
Niki Howe <nikihowe@gmail.com>
2021-06-16 11:12:39 -0400

    Restore from local history

---
+++ config.py
@@ -0,0 +1,23 @@
+import argparse
+import math
+import os
+
+import experiment_buddy
+
+batch_size = 64
+test_batch_size = 1000
+epochs = 14
+lr = 1.0
+gamma = 0.7
+no_cuda = False
+dry_run = False
+seed = 1
+log_interval = 10
+save_model = False
+
+experiment_buddy.register(locals())
+
+################################################################
+# Derivative parameters
+################################################################
+tensorboard = experiment_buddy.deploy(host='mila', sweep_yaml="")
